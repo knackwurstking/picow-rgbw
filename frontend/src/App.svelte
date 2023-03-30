54e19f0d560271c3d893e98dfc4e7c9d67f4f0e0
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-30 10:19:55 +0200

    Update theme links

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,13 +4,13 @@
 <svelte:head>
   <!-- SMUI Light Theme -->
   <link rel="stylesheet"
-    href="/theme/smui.css"
+    href="/smui.css"
     media="(prefers-color-scheme: light)"
   />
 
   <!-- SMUI Dark Theme -->
   <link rel="stylesheet"
-    href="/theme/smui-dark.css"
+    href="/smui-dark.css"
     media="screen and (prefers-color-scheme: dark)"
   />
 
