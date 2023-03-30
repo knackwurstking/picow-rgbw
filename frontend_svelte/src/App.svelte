b45620101dc09cb245b45a619270fff75a4069cf
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-30 12:50:58 +0200

    Adding no smui frontend

--- frontend_svelte/src/App.svelte
+++ frontend_svelte/src/App.svelte
@@ -4,7 +4,14 @@
 <svelte:head>
   <!-- SMUI Light Theme -->
   <link rel="stylesheet"
-    href="/theme.css"
+    href="/smui.css"
+    media="(prefers-color-scheme: light)"
+  />
+
+  <!-- SMUI Dark Theme -->
+  <link rel="stylesheet"
+    href="/smui-dark.css"
+    media="screen and (prefers-color-scheme: dark)"
   />
 
   <title>Pico Web</title>
