2d1ff9d2f753c2af0f602970fae96d2fc70628ee
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-30 10:39:47 +0200

    Add embed FileSystem stuff to frontend

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -1,3 +1,22 @@
 package frontend
 
-// TODO: embed all files in dist
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+)
+
+var (
+	//go:embed dist
+	dist embed.FS
+)
+
+// Dist
+func Dist() http.FileSystem {
+	fsys, err := fs.Sub(dist, "dist")
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
+}
