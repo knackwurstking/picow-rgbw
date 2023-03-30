262679558f07df0c2b9137d9fe0e1937e908a31b
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-30 10:16:08 +0200

    Upgrade frontend to smui

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
 </script>
 
+<svelte:head>
+  <!-- SMUI Light Theme -->
+  <link rel="stylesheet"
+    href="/theme/smui.css"
+    media="(prefers-color-scheme: light)"
+  />
+
+  <!-- SMUI Dark Theme -->
+  <link rel="stylesheet"
+    href="/theme/smui-dark.css"
+    media="screen and (prefers-color-scheme: dark)"
+  />
+
+  <title>Pico Web</title>
+</svelte:head>
+
 <main>
 </main>
 
