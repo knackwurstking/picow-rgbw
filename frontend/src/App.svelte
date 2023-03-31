86f5060323aa959f37700ad252294d8acca5bc0c
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-31 15:03:26 +0200

    Adding todos

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -11,6 +11,14 @@
 </svelte:head>
 
 <main>
+  <!-- TODO: ...
+    ...split view (landscape or horizontal orientation)
+    ...checkbox like group of devices for selecting device(s) to control
+    ...button group for on/off
+    ...input group (range slider) for rgbw (0-100)
+    ...color preview somewhere
+    ...color history (stored on client side)
+  -->
 </main>
 
 <style>
