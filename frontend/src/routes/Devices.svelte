0d354ba626d132902dba76a0be51dff44361e580
Udo Bauer <knackwurstking.tux@gmail.com>
2023-03-31 20:06:51 +0200

    Adding main sections 'list' and 'ctrl'

--- frontend/src/routes/Devices.svelte
+++ frontend/src/routes/Devices.svelte
@@ -16,14 +16,49 @@
 -->
 
 <div class="devices container">
+    <section class="list">
+    </section>
+    <section class="ctrl">
+    </section>
 </div>
 
 <style>
     div.devices.container {
         display: flex;
+        flex-direction: column;
         width: 100%;
         height: 100%;
     }
 
-    /* TODO: handle orientation landscape and portrait via available pixel with */
+    div.devices.container > section {
+        border: 1px solid red; 
+        width: 100%;
+        height: 100%;
+    }
+
+    div.devices.container > section.list {
+        height: 50%;
+    }
+
+    div.devices.container > section.ctrl {
+        height: 50%;
+    }
+
+    /* default: mobile (portrait) */
+
+    @media (min-width: 1025px) {
+        div.devices.container {
+            flex-direction: row;
+        }
+
+        div.devices.container > section.list {
+            width: 50%;
+            height: 100%;
+        }
+
+        div.devices.container > section.ctrl {
+            width: 50%;
+            height: 100%;
+        }
+    }
 </style>
