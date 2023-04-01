fcd974f5469456e1c60d6e6a3747467aebd9f7c9
Udo Bauer <knackwurstking.tux@gmail.com>
2023-04-01 17:15:45 +0200

    Adding simple cors middleware

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -2,18 +2,40 @@ package middleware
 
 import "net/http"
 
-type CORS struct {
-	handler http.Handler
+type CORSOptions struct {
+	AllowOrigin string
 }
 
-func NewCORS(h http.Handler) http.Handler {
+func NewCORSOptions() *CORSOptions {
+	return &CORSOptions{
+		AllowOrigin: "*",
+	}
+}
+
+type CORS struct {
+	handler http.Handler
+	options *CORSOptions
+}
+
+func NewCORS(h http.Handler, options *CORSOptions) http.Handler {
+	if options == nil {
+		options = NewCORSOptions()
+	}
+
 	return &CORS{
 		handler: h,
+		options: options,
 	}
 }
 
 func (c *CORS) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// TODO: ...
+	h := w.Header()
+	h.Set("Access-Control-Allow-Origin", c.options.AllowOrigin)
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 
 	c.handler.ServeHTTP(w, r)
 }
