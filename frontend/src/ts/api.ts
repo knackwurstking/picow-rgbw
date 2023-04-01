cb0f42333c7a7248fe8d1674d8e40a0ac3e5fb8b
Udo Bauer <knackwurstking.tux@gmail.com>
2023-04-01 16:35:27 +0200

    Add devices method and paths data to api class

--- frontend/src/ts/api.ts
+++ frontend/src/ts/api.ts
@@ -8,17 +8,46 @@ export interface Device {
     rgbw: GpPWM[];
 }
 
+export interface ApiPathsV1 {
+    devices: () => string;
+    device: (id: string) => string
+}
+
+export interface ApiPaths {
+    v1: ApiPathsV1
+}
+
 export class Api {
     protocol: ("http:" | "https:")
+    host: string
+    version: ("v1")
+    paths: ApiPaths
 
     constructor() {
         this.protocol = "http:"
+        this.host = "" 
+        this.version = "v1"
+        this.paths = {
+            v1: {
+                devices: () => "/api/v1/devices",
+                device: (id) => `/api/v1/device${id}`,
+            }
+        }
     }
 
-    async get(host: string | null = null): Promise<Device[]> {
-        // TODO: do a "/api/v1/devices" call here parse and return the result
+    url(key: string, ...param: any): string {
+        return `${this.protocol}//${this.host}${this.paths[this.version][key](...param)}`
+    }
 
-        return [];
+    async devices(): Promise<Device[]> {
+        const url = this.url("devices");
+        const resp = await fetch(url);
+
+        if (!resp.ok) {
+            throw `response error: ${resp.statusText} (${url})`
+        }
+
+        return await resp.json() as Device[]
     }
 }
 
