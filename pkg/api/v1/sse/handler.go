50726841ca9f94385eb8a5fbe6c37c0a78d8b214
Udo Bauer <knackwurstking.tux@gmail.com>
2023-04-11 17:30:58 +0200

    Update sse handler (add some methods like add and close)

--- pkg/api/v1/sse/handler.go
+++ pkg/api/v1/sse/handler.go
@@ -1,16 +1,50 @@
 package sse
 
+import (
+	"net/http"
+
+	"github.com/knackwurstking/picow-rgbw-web/pkg/api/v1/pico"
+)
+
 type Connection struct {
+	Request *http.Request
 }
 
-func NewConnection() Connection {
-	return Connection{}
+func NewConnection() *Connection {
+	return &Connection{}
 }
 
 type Handler struct {
-	connections []Connection
+	connections []*Connection
 }
 
 func NewHandler() *Handler {
 	return &Handler{}
+}
+
+func (h *Handler) Add(w http.ResponseWriter, r *http.Request, p *pico.Handler) (conn *Connection, ok bool) {
+	h.headers(w)
+
+	// TODO: create connection
+
+	conn = NewConnection()
+	h.connections = append(h.connections, conn)
+
+	return conn, true
+}
+
+func (h *Handler) Close(conn *Connection) {
+	for i, c := range h.connections {
+		if c == conn {
+			h.connections = append(h.connections[:i], h.connections[i+1:]...)
+			return
+		}
+	}
+}
+
+func (h *Handler) headers(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 }
