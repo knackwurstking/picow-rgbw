3928a7306bea0f4983cfab6a0d23bc5a9c376918
Udo Bauer <knackwurstking.tux@gmail.com>
2023-04-12 05:46:53 +0200

    Add Connections type

--- pkg/api/v1/sse/connection.go
+++ pkg/api/v1/sse/connection.go
@@ -3,6 +3,8 @@ package sse
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/gookit/slog"
 )
 
 type Connection struct {
@@ -25,4 +27,37 @@ func (c *Connection) Write(event string, data any) error {
 	_, err = c.Writer.Write(append([]byte("data: "), d...))
 
 	return err
+}
+
+type Connections map[string][]*Connection
+
+func (cs Connections) Add(e string, c *Connection) {
+	slog.Debug("add sse connection:", c.Request.RemoteAddr)
+
+	if conns, ok := cs[e]; ok {
+		cs[e] = append(conns, c)
+	} else {
+		cs[e] = []*Connection{c}
+	}
+}
+
+func (cs Connections) Get(e string) []*Connection {
+	if conns, ok := cs[e]; ok {
+		return conns
+	}
+
+	return []*Connection{}
+}
+
+func (cs Connections) Remove(e string, c *Connection) {
+	slog.Debug("remove sse connection:", c.Request.RemoteAddr)
+
+	if conns, ok := cs[e]; ok {
+		for i, c2 := range conns {
+			if c2 == c {
+				cs[e] = append(conns[:i], conns[i+1:]...)
+				return
+			}
+		}
+	}
 }
--- pkg/api/v1/sse/handler.go
+++ pkg/api/v1/sse/handler.go
@@ -7,13 +7,11 @@ import (
 )
 
 type Handler struct {
-	connections map[string][]*Connection
+	Connections Connections
 }
 
 func NewHandler() *Handler {
-	return &Handler{
-		connections: make(map[string][]*Connection),
-	}
+	return &Handler{}
 }
 
 // Add a connection to handle
@@ -32,46 +30,23 @@ func (h *Handler) Add(event string, w http.ResponseWriter, r *http.Request) (*Co
 		Flusher: f,
 		Request: r,
 	}
-	h.add(event, c)
+	h.Connections.Add(event, c)
 
 	w.WriteHeader(http.StatusOK)
 	return c, true
 }
 
 func (h *Handler) Close(event string, conn *Connection) {
-	h.remove(event, conn)
+	h.Connections.Remove(event, conn)
 }
 
 func (h *Handler) Dispatch(event string, data any) {
-	if conns, ok := h.connections[event]; ok {
-		for _, c := range conns {
-			go func(c *Connection) {
-				if err := c.Write(event, data); err != nil {
-					slog.Error(err.Error())
-				}
-			}(c)
-		}
-	}
-}
-
-func (h *Handler) add(event string, conn *Connection) {
-	slog.Debug("add sse connection:", conn.Request.RemoteAddr)
-	if conns, ok := h.connections[event]; ok {
-		h.connections[event] = append(conns, conn)
-	} else {
-		h.connections[event] = []*Connection{conn}
-	}
-}
-
-func (h *Handler) remove(event string, conn *Connection) {
-	slog.Debug("remove sse connection:", conn.Request.RemoteAddr)
-	if conns, ok := h.connections[event]; ok {
-		for i, c2 := range conns {
-			if c2 == conn {
-				h.connections[event] = append(conns[:i], conns[i+1:]...)
-				return
+	for _, c := range h.Connections.Get(event) {
+		go func(c *Connection) {
+			if err := c.Write(event, data); err != nil {
+				slog.Error(err.Error())
 			}
-		}
+		}(c)
 	}
 }
 
