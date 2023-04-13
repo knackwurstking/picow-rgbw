f46bd7bf4dc1f70114e3376f0583d9a85fc633a4
Udo Bauer <knackwurstking.tux@gmail.com>
2023-04-13 23:00:47 +0200

    Add todos

--- frontend/src/ts/api.ts
+++ frontend/src/ts/api.ts
@@ -6,12 +6,14 @@ export interface Gp {
     duty: Duty;
 }
 
+// TODO: add "brightness" field?
 export interface Device {
     addr: string;
     offline: boolean;
     rgbw: Gp[];
 }
 
+// TODO: add "brightness" field?
 export interface ReqPutDevice {
     addr: string;
     rgbw: Duty[];
