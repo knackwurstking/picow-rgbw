12b333a9a69a5f99fc7d884ccaa74ce4e6a94c5d
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-01 16:58:32 +0200

    Handle "click" event on button

--- frontend/src/lib/components/custom/button/Button.svelte
+++ frontend/src/lib/components/custom/button/Button.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
   //export let variant: "text" | "raised" | "unelevated" | "outlined" =
   export let variant: "unelevated" | "outlined" = "unelevated";
   export let color: "primary" | "secondary" = "primary";
+
+  const dispatch = createEventDispatcher();
 
   // TODO: add ripple effect, just like smui buttons
 </script>
@@ -10,6 +14,7 @@
   class={`custom-button ${variant} ${color}`}
   style={`` + $$props.style || ""}
   {...$$restProps}
+  on:click={(ev) => dispatch("click", ev)}
 >
   <slot />
 </button>
